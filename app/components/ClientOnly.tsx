dc6e2310b0b7619027214dc722bbec64bca91e7b
Zachariah Ngonyani <zachariahn@gmail.com>
2023-04-08 10:34:01 +0300

    create a client only component for handling bleeding

---
+++ app/components/ClientOnly.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+interface ClientOnlyProps {
+  children: React.ReactNode;
+}
+
+const ClientOnly: React.FC<ClientOnlyProps> = ({ children }) => {
+  const [hasMounted, setHasMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) {
+    return null;
+  }
+
+  return <>{children}</>;
+};
+
+export default ClientOnly;
