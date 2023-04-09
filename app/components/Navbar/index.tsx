b011586948fe840145e221959db6442f5706daac
Zachariah Ngonyani <zachariahn@gmail.com>
2023-04-09 06:32:46 +0300

    addlow navbar to accept a current user prop

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -1,12 +1,14 @@
-'use client';
+"use client";
 
 import React from "react";
 import Container from "@/app/components/Container";
 import Logo from "@/app/components/Navbar/Logo";
 import Search from "@/app/components/Navbar/Search";
 import UserMenu from "@/app/components/Navbar/UserMenu";
+import CurrentUser from "@/app/types/current-user";
+import Category from "@/app/components/Navbar/Category";
 
-const Navbar = (props: {}) => {
+const Navbar: React.FC<CurrentUser> = ({ currentUser }) => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 borde-b-[1px]">
@@ -14,10 +16,11 @@ const Navbar = (props: {}) => {
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
             <Logo />
             <Search />
-            <UserMenu />
+            <UserMenu currentUser={currentUser} />
           </div>
         </Container>
       </div>
+      <Category />
     </div>
   );
 };
