3be8c582144446e193d60141ec87a75455c78bc9
Zachariah Ngonyani <zachariahn@gmail.com>
2023-04-10 10:14:25 +0300

    add listing card to page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,47 @@
-const Home = () => {
-  return <div className="text-rose-500 text-2xl">Hello Airbnb</div>;
+import Container from "@/app/components/Container";
+import ClientOnly from "@/app/components/ClientOnly";
+import EmptyState from "@/app/components/EmptyState";
+import getListings from "@/app/actions/getListings";
+import ListingCard from "@/app/components/Listing/Card";
+import { Listing } from "@prisma/client";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import CurrentUser from "@/app/types/current-user";
+
+const Home = async () => {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <Container>
+        <div
+          className="
+            pt-24
+            grid
+            grid-cols-1
+            sm:grid-cols-2
+            md:grid-cols-3
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+            "
+        >
+          {listings.map((listing: Listing, index: number) => (
+            <ListingCard key={index} data={listing} currentUser={currentUser as CurrentUser} />
+          ))}
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 };
 
 export default Home;
