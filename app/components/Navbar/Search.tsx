e2d0b784fe10f6ae19860a36354747a4fab8e238
Zachariah Ngonyani <zachariahn@gmail.com>
2023-04-11 09:46:19 +0300

    re-format navbar search component, hook it up with appropriate functions

--- app/components/Navbar/Search.tsx
+++ app/components/Navbar/Search.tsx
@@ -1,16 +1,65 @@
 "use client";
 
+import useSearchModal from "@/app/hooks/useSearchModal";
 import { BiSearch } from "react-icons/bi";
 
 const Search = () => {
+  const searchModal = useSearchModal();
   return (
-    <div className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-sm transition cursor-pointer">
-      <div className="flex flex-row items-center justify-between">
-        <div className="text-sm font-semibold px-6">search...</div>
-        <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
+    <div
+      onClick={searchModal.onOpen}
+      className="
+      border-[1px] w-full 
+      md:w-auto
+      py-2
+      rounded-full
+      shadow-sm
+      hover:shadow-sm
+      transition 
+      cursor-pointer"
+    >
+      <div
+        className="
+        flex
+        flex-row
+        items-center
+        justify-between
+        "
+      >
+        <div
+          className="
+          text-sm 
+          font-semibold 
+          px-6
+          "
+        >
+          search...
+        </div>
+        <div
+          className="
+          hidden
+          sm:block
+          text-sm 
+          font-semibold 
+          px-6 border-x-[1px]
+          flex-1 
+          text-center
+          "
+        >
           any week
         </div>
-        <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
+        <div
+          className="
+          text-sm 
+          pl-6 
+          pr-2 
+          text-gray-600 
+          flex 
+          flex-row 
+          items-center 
+          gap-3
+          "
+        >
           <div className="hidden sm:block">Add Guests</div>
           <div className="p-2 bg-rose-500 rounded-full text-white">
             <BiSearch size={18} />
