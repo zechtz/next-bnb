d009b35e183c2d278815049d8899860f031c494e
Zachariah Ngonyani <zachariahn@gmail.com>
2023-04-11 09:42:15 +0300

    add search params to home page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import Container from "@/app/components/Container";
 import ClientOnly from "@/app/components/ClientOnly";
 import EmptyState from "@/app/components/EmptyState";
-import getListings from "@/app/actions/getListings";
+import getListings, { IListingParams } from "@/app/actions/getListings";
 import ListingCard from "@/app/components/Listing/Card";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import { SafeListing } from "@/app/types";
 
-const Home = async () => {
-  const listings = await getListings();
+interface HomeProps {
+  searchParams: IListingParams,
+}
+
+const Home = async ({searchParams} : HomeProps) => {
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
