a6c137d3fa0ed0ace45a373ad9dc0bcb574f7146
Zachariah Ngonyani <zachariahn@gmail.com>
2023-04-11 09:42:57 +0300

    create reservatiosn component

---
+++ app/reservations/page.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import ClientOnly from "@/app/components/ClientOnly";
+import EmptyState from "@/app/components/EmptyState";
+import getReservations from "@/app/actions/getReservations";
+import ReservationClient from "@/app/reservations/ReservationClient";
+
+const ReservationsPage = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return (
+      <ClientOnly>
+        <EmptyState title="Unauthorized" subtitle="Please login" />
+      </ClientOnly>
+    );
+  }
+
+  const reservations = await getReservations({ authorId: currentUser.id.toString() });
+
+  if (reservations.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No reservations found"
+          subtitle="Looks like you have no reservations on your properties."
+        />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <ReservationClient
+        reservations={reservations}
+        currentUser={currentUser}
+      />
+    </ClientOnly>
+  );
+};
+
+export default ReservationsPage;
