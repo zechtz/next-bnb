ab4eeab1a857d8a8fd21a593a3c286d1c171f7f3
Zachariah Ngonyani <zachariahn@gmail.com>
2023-04-11 09:41:11 +0300

    update types, add safe reservations

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,19 +1,19 @@
-import { Listing, /* Reservation, */ User } from "@prisma/client";
+import { Listing, Reservation, User } from "@prisma/client";
 
 export type SafeListing = Omit<Listing, "createdAt" | "updatedAt"> & {
   createdAt: string;
   updatedAt: string;
 };
 
-// export type SafeReservation = Omit<
-//   Reservation,
-//   "createdAt" | "startDate" | "endDate" | "listing"
-// > & {
-//   createdAt: string;
-//   startDate: string;
-//   endDate: string;
-//   listing: SafeListing;
-// };
+export type SafeReservation = Omit<
+  Reservation,
+  "createdAt" | "updatedAt" | "startDate" | "endDate" | "listing"
+> & {
+  createdAt: string;
+  startDate: string;
+  endDate: string;
+  listing: SafeListing;
+};
 
 export type SafeUser = Omit<
   User,
